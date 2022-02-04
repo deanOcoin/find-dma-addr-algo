6940027e90716fda03c6982df605f695e057ee2a
deanOcoin <77565607+deanOcoin@users.noreply.github.com>
2022-02-04 14:38:18 -0500

    Update main.cpp
    better var names

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@
 
 // Find DMA Address Algo | C++/x86 MASM
 
-uintptr_t FindDMAAddress(uintptr_t ptr, std::vector<uint32_t> offsets)
+uintptr_t FindDMAAddress(uintptr_t base, std::vector<uint32_t> offset_vec)
 {
-    uintptr_t addr = ptr;
-    int vecsize = offsets.size();
+    uintptr_t addr = base;
+    int vecsize = offset_vec.size();
     int i = 0;
 
     __asm
     {
     addl:
         mov addr, [addr]
-        add addr, offsets[i]
+        add addr, offset_vec[i]
         add i, 1
         cmp i, vecsize
         jl addl
